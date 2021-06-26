ffd2673ba6f503b1213294438cba4937bd498a88
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-06-26 19:35:48 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+for (let i = 0; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+    if (i = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]) {
+        console.log("Output: Number " + i + "is even");
+    } else {
+        console.log("Output: Number " + i + "is odd");
+    }
+};
