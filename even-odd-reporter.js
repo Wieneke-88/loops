baa33d91a225840c401af642f52ab30b35c30a7c
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-06-26 19:55:26 +0200

    Add files via upload

--- even-odd-reporter.js
+++ even-odd-reporter.js
@@ -1,10 +1,19 @@
 console.log("hello world");
-
+/*
+//Mijn oplossing 
 for (let i = 0; i <= 20; i++) {
-    console.log(`Is ${i} even or odd?`);
     if (i = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]) {
         console.log("Output: Number " + i + "is even");
     } else {
         console.log("Output: Number " + i + "is odd");
     }
 };
+*/
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even");
+    } else {
+        console.log(i + " is odd");
+    }
+}
+
